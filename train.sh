0833376634f1d7ad952bb2cf5bd6f4f1560f686a
EC2 Default User <ec2-user@ip-172-31-55-103.ec2.internal>
2024-09-29 07:05:27 +0000

    fix inference_singleOutput

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 # torchrun --nproc_per_node 1 --nnodes 1 finetune/finetune_fully.py
 # torchrun --nproc_per_node 1 --nnodes 1 finetune/lora_tune.py
 
-torchrun --nproc_per_node 4 --nnodes 1 finetune/finetune_fully.py
-# torchrun --nproc_per_node 4 --nnodes 1 finetune/lora_tune.py
+torchrun --nproc_per_node 8 --nnodes 1 finetune/finetune_fully.py
+# torchrun --nproc_per_node 8 --nnodes 1 finetune/lora_tune.py
