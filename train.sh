314ad9f35919dba07f7afd59b1833240a447b0ef
Ubuntu <ubuntu@ip-172-31-52-2.us-west-2.compute.internal>
2024-11-05 09:02:40 +0000

    fix loss bug

--- train.sh
+++ train.sh
@@ -5,6 +5,8 @@ torchrun --nproc_per_node 8 --nnodes 1 finetune/finetune_fully.py
 # torchrun --nproc_per_node 8 --nnodes 1 finetune/lora_tune.py
 
 #  --num_workers 8
-#  --load_pretrained True
+#  --load_pretrained False
+#  --load_my_best False
 #  --visualize True
 #  --only_test True
+#  --only_use_wind_speed_loss True
