2b409c8472e6bc105ba106844db5547096c6cbe9
Ubuntu <ubuntu@ip-172-31-52-2.us-west-2.compute.internal>
2024-11-08 07:38:54 +0000

    change loss precision

--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@ torchrun --nproc_per_node 8 --nnodes 1 finetune/finetune_fully.py
 # torchrun --nproc_per_node 8 --nnodes 1 finetune/lora_tune.py
 
 #  --num_workers 8
-#  --load_pretrained False
+#  --load_pretrained True
 #  --load_my_best False
 #  --visualize True
 #  --only_test True
