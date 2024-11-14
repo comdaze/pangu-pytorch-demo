3449b54de184882b6d9fe0f1e8bed27a5b76fdb0
Ubuntu <ubuntu@ip-172-31-88-222.ec2.internal>
2024-11-14 06:37:50 +0000

    add deepspped

--- train.sh
+++ train.sh
@@ -1,8 +1,10 @@
 # torchrun --nproc_per_node 1 --nnodes 1 finetune/finetune_fully.py
 # torchrun --nproc_per_node 1 --nnodes 1 finetune/lora_tune.py
 
-torchrun --nproc_per_node 8 --nnodes 1 finetune/finetune_fully.py
+# torchrun --nproc_per_node 8 --nnodes 1 finetune/finetune_fully.py
 # torchrun --nproc_per_node 8 --nnodes 1 finetune/lora_tune.py
+
+deepspeed --num_gpus=4 finetune/finetune_fully.py --use_deepspeed True
 
 #  --num_workers 8
 #  --load_pretrained True
@@ -10,3 +12,4 @@ torchrun --nproc_per_node 8 --nnodes 1 finetune/finetune_fully.py
 #  --visualize True
 #  --only_test True
 #  --only_use_wind_speed_loss True
+#  --use_deepspeed True
