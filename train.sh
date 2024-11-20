38e26a585a02674ac03bbc3b01c591fbd122e63d
Ubuntu <ubuntu@ip-172-31-34-24.cn-northwest-1.compute.internal>
2024-11-20 09:08:56 +0000

    fix deepspeed bug

--- train.sh
+++ train.sh
@@ -1,12 +1,13 @@
 # torchrun --nproc_per_node 1 --nnodes 1 finetune/finetune_fully.py
 # torchrun --nproc_per_node 1 --nnodes 1 finetune/lora_tune.py
 
-torchrun --nproc_per_node 8 --nnodes 1 finetune/finetune_fully.py
+# torchrun --nproc_per_node 8 --nnodes 1 finetune/finetune_fully.py
 # torchrun --nproc_per_node 8 --nnodes 1 finetune/lora_tune.py
 
 # deepspeed --num_gpus=8 models/pangu_model_deepspeed.py 
-# deepspeed --num_gpus=8 finetune/finetune_fully.py --use_deepspeed True
+deepspeed --num_gpus=8 finetune/finetune_fully.py --use_deepspeed True
 
+#  --num_workers 4
 #  --num_workers 8
 #  --load_pretrained True
 #  --load_my_best False
